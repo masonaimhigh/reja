ef3209543903de1dd5093ac3edf9e31d65dfdafa
masonaimhigh <mvlogs054@gmail.com>
2024-05-23 18:49:13 +0900

    feat: MongoDB ulanish logic

--- server.js
+++ server.js
@@ -1,54 +1,28 @@
-console.log("Web serverni boshlash");
-const express = require("express");
-const app = express();
 const http = require("http");
-const fs = require("fs");
-
-let user;
-fs.readFile("database/user.json", "utf8", (err, data) => {
-  if (err) {
-    console.log("ERROR:", err);
-  } else {
-    user = JSON.parse(data);
+const mongodb = require("mongodb");
+let db;
+const connectionString =
+  "mongodb+srv://mason:Mcalbert23@cluster0.haiew6q.mongodb.net/Reja";
+mongodb.connect(
+  connectionString,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err, client) => {
+    if (err) console.log("ERROR on connection to MongoDB");
+    else {
+      console.log("MongoDB connection succeed");
+      module.exports = client;
+      //console.log(client);
+      const app = require("./app");
+      const server = http.createServer(app);
+      let PORT = 3000;
+      server.listen(PORT, function () {
+        console.log(
+          `The server is running succesfully on port: ${PORT}, http://localhost:${PORT}`
+        );
+      });
+    }
   }
-});
-
-//1: Kirish kodlari
-app.use(express.static("public"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-//2:Session code
-
-//3: View code
-// BCCR - backend da view, fronted yasaymiz, bu tradational usul - backend html yasab climate ga yuboramiz
-app.set("views", "views");
-app.set("view engine", "ejs");
-
-//4: Routing code
-// app.get("/hello", function (req, res) {
-//   res.end(`<h1 style= "background: red">Hello World </h1>`);
-// });
-
-// app.get("/gift", function (req, res) {
-//   res.end(`<h1>Siz sovgalar bolimidasiz</h1>`);
-// });
-app.post("/create-item", (req, res) => {
-  console.log(req.body);
-  res.json({ test: "success" });
-});
-
-app.get("/author", (req, res) => {
-  res.render("author", { user: user });
-});
-
-app.get("/", (req, res) => {
-  res.render("reja");
-});
-const server = http.createServer(app);
-let PORT = 3000;
-server.listen(PORT, function () {
-  console.log(
-    `The server is running succesfully on port: ${PORT}, http://localhost:${PORT}`
-  );
-});
+);
