eadc24e3ff4e4634c7f35573098bb08c0511d6a7
masonscript4 <heymino68@gmail.com>
2025-06-01 01:54:31 +0900

    BRR: initial Commit

---
+++ server.js
@@ -0,0 +1,31 @@
+console.log("Web Serverni boshlash");
+const express = require("express");
+const app = express();
+const http = require("http");
+
+
+// 1 Kirish code
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({extended:true})); // html form recieved
+
+//2 Session
+
+//3 Views - BSSR backenda frontend 
+app.set('views', "views");
+app.set("view engine", "ejs");
+
+//4 Routing
+app.get("/hello", function(req, res){
+    res.end("Hello World");
+}); 
+
+app.get("/gift", function(req, res){
+    res.end("Siz sovgalar bolimidasiz");
+}); 
+
+const server = http.createServer(app);
+let PORT = 3000;
+server.listen(PORT, function(){
+    console.log(`The server is running successfully on port: ${PORT}`);
+})
