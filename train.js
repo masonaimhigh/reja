dc7589d2d97ccbdce78245f6b55f24e2dd6bb3cd
masonscript4 <heymino68@gmail.com>
2025-06-06 21:03:55 +0900

    NodeJS event loop va Callback functionlar

--- train.js
+++ train.js
@@ -1,1 +1,36 @@
-console.log("Train task");
+// console.log("Train task");
+
+console.log("Jack Ma maslahatlari");
+const list = [
+  "yaxshi talaba boling", // 0-20
+  "togri boshliq tanlang va koproq hato qiling", //20-30
+  "uzingizga ishlashni boshlang", //30-40
+  "siz kuchli bolgan narsalarni qiling", //40-50
+  "yoshlarga investitsiya qiling", //50-60
+  "endi dam oling", //60
+];
+
+function maslahatBering(a, callback) {
+  if (typeof a !== "number") callback("Insert number", null);
+  else if (a <= 20) callback(null, list[0]);
+  else if (a > 20 && a <= 30) callback(null, list[1]);
+  else if (a > 30 && a <= 40) callback(null, list[2]);
+  else if (a > 40 && a <= 50) callback(null, list[3]);
+  else if (a > 50 && a <= 60) callback(null, list[4]);
+  else {
+   //setTimeout
+    setTimeout(function () {
+      callback(null, list[5]);
+    }, 5000);
+  }
+}
+
+console.log("passed here 0");
+maslahatBering(65, (err, data) => {
+  if (err) console.log("ERROR:", err);
+  else {
+    console.log("javob:", data);
+  }
+});
+console.log("passed here 1");
+
