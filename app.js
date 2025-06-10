174295a4ff66577962b942e880e8d42e407148ca
masonscript4 <heymino68@gmail.com>
2025-06-11 02:43:38 +0900

    feat: CRUD operations

--- app.js
+++ app.js
@@ -30,7 +30,15 @@ app.set("view engine", "ejs");
 //4: Routing code
 app.post("/create-item", (req, res) => {
   console.log(req.body);
-  res.json({ test: "success" });
+  const new_reja = req.body.reja;
+  db.collection('plans').insertOne({reja: new_reja}, (err, data) => {
+    if(err) {
+      console.log(err);
+      res.end("Something went wrong")
+    }else {
+      res.end("successfully added")
+    }
+  });
 });
 
 app.get("/author", (req, res) => {
@@ -38,13 +46,15 @@ app.get("/author", (req, res) => {
 });
 
 app.get("/", (req, res) => {
-db.collection("plans").find().toArray((err, data) => {
+db.collection("plans")
+ .find()
+ .toArray((err, data) => {
   if(err) {
     console.log(err);
     res.end("something went wrong")
   } else {
     console.log(data);
-    res.render("reja");
+    res.render("reja", {items: data});
   }
 });
 });
